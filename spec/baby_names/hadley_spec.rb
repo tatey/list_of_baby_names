aeed84f22565e59c166b25f39be510d32c546fa4
Tate Johnson <tate@tatey.com>
2017-11-28 20:00:16 +1000

    Move hadley spec into its own file.

--- spec/baby_names/hadley_spec.rb
+++ spec/baby_names/hadley_spec.rb
@@ -2,24 +2,24 @@ require "spec_helper"
 
 RSpec.describe BabyNames::Hadley do
   before(:all) do
-    @hadley = BabyNames::Hadley.new
+    @data_set = BabyNames::Hadley.new
   end
 
   describe "#names" do
     it "has John as the first name" do
-      name = @hadley.names.first
+      name = @data_set.names.first
       expect(name.name).to eq("John")
       expect(name.gender).to eq(:male)
     end
 
     it "has Elianna as the last name" do
-      name = @hadley.names.last
+      name = @data_set.names.last
       expect(name.name).to eq("Elianna")
       expect(name.gender).to eq(:female)
     end
 
     it "has 258,000 names" do
-      expect(@hadley.names.size).to eq(258_000)
+      expect(@data_set.names.size).to eq(258_000)
     end
   end
 end
